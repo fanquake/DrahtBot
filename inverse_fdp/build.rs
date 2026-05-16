9160aed70378a6ee2ae5466f62c38d4b0eb19c2f
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2026-05-16 08:23:28 +0200

    inverse_fdp: Move main logic from test into main function

--- inverse_fdp/build.rs
+++ inverse_fdp/build.rs
@@ -1,7 +1,7 @@
 // Requires:
-// cargo clean --package=inverse_fdp && cargo test
+// cargo clean --package=inverse_fdp && cargo test && cargo run
 //
-// But first, insert the generated code below the comment `// Insert below` in the test
+// But first, insert the generated code below the comment `// Insert below` in the main function
 extern crate cpp_build;
 fn main() {
     cpp_build::build("src/main.rs");
